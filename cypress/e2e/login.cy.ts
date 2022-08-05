37bc1737016eee3ccee9f72b715e11173c624fc8
Luã Govinda <govinda777@gmail.com>
2022-08-05 20:32:03 -0300

    up

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,12 +1,18 @@
 
 
-describe('Test case - Login', () => {
-  beforeEach(() => {
-    cy.visit('/')
+describe('Test describe - ABC', {},  () => {
+
+  context('Test context - ABC', {}, () => {
+
+    beforeEach(() => {
+      cy.visit('/')
+    })
+
+    it("Teste it - ABC", {},  () => {
+
+
+    })
+
   })
 
-  it("Teste", () => {
-
-    console.log("vjhgjhgjkghjk");
-  })
 })

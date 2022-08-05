094043798aad54511ef32d7850cf4b98726f961c
Luã Govinda <govinda777@gmail.com>
2022-08-05 19:57:19 -0300

    up

---
+++ cypress/integration/login.cy.ts
@@ -0,0 +1,13 @@
+
+
+describe('Test case - Login', () => {
+  beforeEach(() => {
+    cy.visit('https://www.homolog.backoffice.merx.tech/')
+  })
+
+  it("Teste", () => {
+
+    
+    console.log("vjhgjhgjkghjk");
+  })
+})
